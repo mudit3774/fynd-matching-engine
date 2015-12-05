8282cf4f19c5637a43bc80d902b6387aef799923
Mudit Raaj Gupta <mudit.raaj.gupta@gmail.com>
2015-12-05 18:25:58 +0530

    Added a reducer

--- geo_helper.py
+++ geo_helper.py
@@ -9,6 +9,4 @@ class GeoHelper:
 	def get_polygon_intersection(self, polygons):
 		shapely_polygons = map(lambda polygon : Polygon(
 			map(lambda x: tuple(x), polygon[0])), polygons)
-		for i in range(1, len(shapely_polygons)):
-			shapely_polygons[0].intersects(shapely_polygons[i])
-		return shapely_polygons[0]
+		return reduce(lambda x,y: x.intersects(y), shapely_polygons)
